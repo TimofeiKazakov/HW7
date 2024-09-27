fcbb06e78ceaf31c21d38dd022b1f9c8442d2a0a
TimofeiKazakov <violet09502@gmail.com>
2024-09-27 16:30:32 +0300

    Add files via upload

---
+++ module_1_6.py
@@ -0,0 +1,14 @@
+my_dict = {'Dmitry': 1990, 'Grigori': 1985, 'Alisa': 2005}
+print(my_dict)
+my_dict.update({'Maria': 1999,
+                'Kirill': 2000})
+print(my_dict)
+my_dict.pop('Dmitry')
+print(my_dict)
+
+my_set = {2, 'Python',  9, 0, 2, 0, 9}
+print(my_set)
+my_set.update({'Java', 8})
+print(my_set)
+my_set.remove(2)
+print(my_set)
